664acd848dabeb61c919a4c26ca56b55b80af937
teodor.zet00@gmail.com <teodor.zet00@gmail.com>
2022-11-18 15:54:44 +0100

    Initial commit

--- BugHunting/src/Main.java
+++ BugHunting/src/Main.java
@@ -5,17 +5,22 @@ import java.util.Arrays;
 public class Main {
 
     @Override
-    public void putToCache(PutRecordsRequest putRecordsRequest){
-        ....
-        if (!dataTmpFile.exists()){
-            try {
-                dataTmpFile.createNewFile();
-            } catch (IOException e) {
-                LOGGER.error("Failed to create cache tmp file, return.", e);
-                return;
-            }
-        }
-        ....
+    public boolean equals(Object o) {
+        CheckpointStatistics that = (CheckpointStatistics) o;
+        return id == that.id &&
+                savepoint == that.savepoint &&
+                triggerTimestamp == that.triggerTimestamp &&
+                latestAckTimestamp == that.latestAckTimestamp &&
+                stateSize == that.stateSize &&
+                duration == that.duration &&
+                alignmentBuffer == that.alignmentBuffer &&
+                processedData == processedData &&
+                persistentData == that.persistentData &&
+                numSubtasks == that.numSubtasks &&
+                numAckSubtasks = that.numAckSubtasks &&
+                status == that.status &&
+                Objects.equals(checkpointType, that.chechpointType) &&
+                Objects.equals(checkpointStatisticsPerTask, that.checkpointStatisticsPerTask);
     }
 
     public static void main(String[] args) {

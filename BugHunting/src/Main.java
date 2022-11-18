eb25202096ed70609fc97d95f635b3f2c2cf9902
teodor.zet00@gmail.com <teodor.zet00@gmail.com>
2022-11-18 12:58:48 +0100

    Initial commit

--- BugHunting/src/Main.java
+++ BugHunting/src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
 
-    private boolean checkDimension(CLIQUEUnit other, int e){
-        for (int i=0,j=0; i<e; i++, j+=2){
-            if (dims[i] != other.dims[i]
-            || bounds[j] != other.bounds[j]
-            || bounds[j+1] != other.bounds[j+1]) {
-                return false;
-            }
-        }
-        return true;
+    @Override
+    public double[] computeMean(){
+        // Not supported except for singletons.
+        return points.size() == 1 ? points.get(1) : null;
     }
 
     public static void main(String[] args) {

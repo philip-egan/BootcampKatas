73eeda494fc4a25eced30bfb2511dd64b0bcf68c
teodor.zet00@gmail.com <teodor.zet00@gmail.com>
2022-11-18 12:54:45 +0100

    Initial commit

---
+++ BugHunting/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    private boolean checkDimension(CLIQUEUnit other, int e){
+        for (int i=0,j=0; i<e; i++, j+=2){
+            if (dims[i] != other.dims[i]
+            || bounds[j] != other.bounds[j]
+            || bounds[j+1] != bounds[j+1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
